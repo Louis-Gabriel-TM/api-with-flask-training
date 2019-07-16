79424e557b4bd2e48f9fec029d16e6734d212380
Louis Gabriel <louis.gabriel@orange.fr>
2019-07-16 21:27:29 +0200

    Feature: blueprint implementation of an API with Flask

--- app.py
+++ app.py
@@ -4,9 +4,47 @@ from flask import Flask
 app = Flask(__name__)  # __name__ is the unique name of this file
 
 
-@app.route('/')  # the root URL of the application
-def home():  # the view associated to this route
-    return "Hello, world!"
+# POST - used to received data (in a server point of view)
+# GET - used to send data back only (in a server point of view)
+
+# data blueprint
+stores = [
+    {
+        'name': 'A Blueprint Store',
+        'items': [
+            {
+                'name': 'a blueprint item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+# POST /store data: {name:} --> create a new store
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+# POST /store/<string:name>/item data: {name:, price:}
+#     --> create an item in a specific store
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+# GET /store/<string:name> --> get data about a store
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+# GET /store --> get data of all stores
+@app.route('/store')
+def get_stores():
+    pass
+
+# GET /store/<string:name>/item --> get data of all items in a specific store
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
 
 
 app.run(port=5000)  # launch the app on port 5000 (default value)
