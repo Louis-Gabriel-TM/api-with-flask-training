ebf794194c7b882c32b0e14018d03c5de0fffbe2
Louis Gabriel <louis.gabriel@orange.fr>
2019-07-23 15:28:51 +0200

    Feature: add authentication with Flask-JWT

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 
 app = Flask(__name__)
+app.secret_key = 'a_secret_key_to_keep_secret...'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # create /auth
 
 items = []
 
 
 class Item(Resource):
 
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
