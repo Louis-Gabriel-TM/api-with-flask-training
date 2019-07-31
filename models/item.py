16a22816b171c98f96ea473cf0898d1b9ac402a4
Louis Gabriel <louis.gabriel@orange.fr>
2019-07-31 18:23:47 +0200

    Feature: add Store resource and its API

--- models/item.py
+++ models/item.py
@@ -4,6 +4,7 @@ from db import db
 class ItemModel(db.Model):
 
     __tablename__ = 'Items'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
--- resources/item.py
+++ resources/item.py
@@ -11,13 +11,13 @@ class Item(Resource):
         'price',
         type=float,
         required=True,
-        help="This field cannot be left blank"
+        help="This field cannot be left blank."
     )
     parser.add_argument(
         'store_id',
         type=int,
         required=True,
-        help="Every item needs a store id"
+        help="Every item needs a store id."
     )
 
     @jwt_required()
@@ -25,26 +25,24 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
         if item:
             return item.json()
-        return {'message': "Item not found"}, 404
+
+        return {'message': "Item not found."}, 404
 
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': f"An item with name '{name}' already exists."}, 400
 
         data = Item.parser.parse_args()
-
         item = ItemModel(name, **data)
-
         try:
             item.save_to_db()
         except:
-            return {'message': "An error occured inserting the item"}, 500
+            return {'message': "An error occured inserting the item."}, 500
 
         return item.json(), 201
 
     def put(self, name):
         data = Item.parser.parse_args()
-
         item = ItemModel.find_by_name(name)
 
         if item:
@@ -54,7 +52,6 @@ class Item(Resource):
             item = ItemModel(name, **data)
 
         item.save_to_db()
-
         return item.json(), 201
 
     def delete(self, name):
@@ -62,7 +59,7 @@ class Item(Resource):
         if item:
             item.delete_from_db()
 
-        return {'message': 'Item deleted'}
+        return {'message': "Item deleted."}
 
 
 class ItemList(Resource):
