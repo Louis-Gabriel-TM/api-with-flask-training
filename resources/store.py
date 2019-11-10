af151771ac3374983f677a4cf91f342d9074dcd6
Louis Gabriel <louis.gabriel@orange.fr>
2019-11-10 04:19:27 +0100

    Refactor: start code of JWT-extended section

--- resources/store.py
+++ resources/store.py
@@ -1,13 +1,16 @@
 from flask_restful import Resource
+
 from models.store import StoreModel
 
 
 class Store(Resource):
+
     def get(self, name):
         store = StoreModel.find_by_name(name)
         if store:
             return store.json()
-        return {'message': 'Store not found'}, 404
+
+        return {'message': 'Store not found.'}, 404
 
     def post(self, name):
         if StoreModel.find_by_name(name):
@@ -26,9 +29,10 @@ class Store(Resource):
         if store:
             store.delete_from_db()
 
-        return {'message': 'Store deleted'}
+        return {'message': 'Store deleted.'}
 
 
 class StoreList(Resource):
+
     def get(self):
-        return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
+        return {'stores': [store.json() for store in StoreModel.find_all()]}
